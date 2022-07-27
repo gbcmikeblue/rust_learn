36f3ebd7a3e1b3929b2cc66248846634bc2ae745
gaobicheng <1061589622@qq.com>
2022-07-27 19:00:18 +0800

    first rust code

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 record learn rust~
 
 # 这里记录学习rust的过程
+
+## 通过例子学 Rust
+
+### 1. hellow world
+
+代码路径 [传送至这里](https://github.com/gbcmikeblue/rust_learn/blob/main/README.md)
---
+++ code/1.hello_world/hello_world.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+
+    // println! 是一个宏 输出 console
+    // 使用 rustc hello_world.trs 编译
+    // rustc --out-dir ./exec_files ./code/1.hello_world/hello_world.rs
+    // 得到可执行文件 hello_world 即可运行
+    println!("Hello World! Hello Rust!");
+    println!("It's time to go home!");
+}
